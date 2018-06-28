030fdfbb7219e142f152072d869367a1d17c4d31
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-28 14:33:45 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+.generateCardValues(){
+    var cardArray=[]
+    var i;
+    for (i=1, i<9, i++){
+      cardArray.push(i);
+      cardArray.push(i);
+    }
+    var cardArrayRandom[]
+    while (cardArray.length>0) {
+
+    }
+}
+
+
+  });
