2783aaf969e67a41ae7dd3bba871332cb83bf137
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-29 12:52:44 -0400

    Third commit

--- js/main.js
+++ js/main.js
@@ -8,13 +8,30 @@ $(document).ready(function(){
       cardArray.push(i);
       cardArray.push(i);
     }
-    var cardArrayRandom[]
+    var cardArrayRandom=[];
     while (cardArray.length>0) {
-    var randomDecimal=Math.random()*16;
-    var randomInteger=Math.floor(randomDecimal);
+    var randomInteger=Math.floor(Math.random()*16);
     var randomNumber=cardArray[randomInteger];
     cardArrayRandom.push(randomNumber);
     cardArray.splice(randomInteger, 1);
     }
+}
+.renderCards(cardValues){
+  cardColors=["hsl(25, 85%, 65%)",
+              "hsl(55, 85%, 65%)",
+              "hsl(90, 85%, 65%)",
+              "hsl(160, 85%, 65%)",
+              "hsl(220, 85%, 65%)",
+              "hsl(265, 85%, 65%)",
+              "hsl(310, 85%, 65%)",
+              "hsl(360, 85%, 65%)",
+            ];
+  $('#game').empty();
+  var i;
+  for (i=0, i<cardArrayRandom.length, i++){
+    $("#game").data(newCardObject, cardArrayRandom[i]);
+    $(".card").data(false);
+    $(".card").data(cardColors[x]);
+  }
 }
 });
