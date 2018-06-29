6555c1e3848a037aabc422d927f002fa9fe79f43
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-29 19:30:39 -0400

    Oh god fuck my life

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
+var MatchGame={}
 
-
-.generateCardValues(){
+MatchGame.generateCardValues(){
     var cardArray=[]
     var i;
     for (i=1, i<9, i++){
       cardArray.push(i);
       cardArray.push(i);
     }
-    var cardArrayRandom=[];
+    var cardValues=[];
     while (cardArray.length>0) {
     var randomInteger=Math.floor(Math.random()*16);
     var randomNumber=cardArray[randomInteger];
-    cardArrayRandom.push(randomNumber);
+    cardValues.push(randomNumber);
     cardArray.splice(randomInteger, 1);
     }
 }
-.renderCards(cardValues){
-  cardColors=["hsl(25, 85%, 65%)",
+
+  MatchGame.renderCards=function(cardValues, $game){
+    colors=[
+              "hsl(25, 85%, 65%)",
               "hsl(55, 85%, 65%)",
               "hsl(90, 85%, 65%)",
               "hsl(160, 85%, 65%)",
@@ -26,12 +28,13 @@ $(document).ready(function(){
               "hsl(310, 85%, 65%)",
               "hsl(360, 85%, 65%)",
             ];
-  $('#game').empty();
-  var i;
-  for (i=0, i<cardArrayRandom.length, i++){
-    $("#game").data(newCardObject, cardArrayRandom[i]);
-    $(".card").data(false);
-    $(".card").data(cardColors[x]);
+    $("#game").empty();
+    var i;
+    for (i=0, i>cardValues.length, i++){
+      var $newCard=$('<div class="col-xs-3 card"></div>');
+      $newCard.data(value, cardValue[i]);
+      $newCard.data(flipped, false);
+      $newCard.data(color,(value-1) )
+    }
   }
-}
 });
