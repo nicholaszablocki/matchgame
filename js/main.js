d06bd4c915db12657fff99a25a39726dab305369
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-07-03 10:55:15 -0400

    Almost done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 var MatchGame={}
+MatchGame.generateCardValues();
+MatchGame.renderCards(cardValues, $game);
+});
 
-MatchGame.generateCardValues(){
+
+MatchGame.generateCardValues=function(){
     var cardArray=[]
     var i;
     for (i=1, i<9, i++){
@@ -16,25 +20,57 @@ MatchGame.generateCardValues(){
     cardArray.splice(randomInteger, 1);
     }
 }
-
-  MatchGame.renderCards=function(cardValues, $game){
-    colors=[
-              "hsl(25, 85%, 65%)",
-              "hsl(55, 85%, 65%)",
-              "hsl(90, 85%, 65%)",
-              "hsl(160, 85%, 65%)",
-              "hsl(220, 85%, 65%)",
-              "hsl(265, 85%, 65%)",
-              "hsl(310, 85%, 65%)",
-              "hsl(360, 85%, 65%)",
-            ];
-    $("#game").empty();
-    var i;
-    for (i=0, i>cardValues.length, i++){
-      var $newCard=$('<div class="col-xs-3 card"></div>');
-      $newCard.data(value, cardValue[i]);
-      $newCard.data(flipped, false);
-      $newCard.data(color,(value-1) )
+MatchGame.renderCards=function(cardValues, $game){
+  $game.data(flippedCards, []);
+  colors=[
+            hsl(25, 85%, 65%),
+            hsl(55, 85%, 65%),
+            hsl(90, 85%, 65%),
+            hsl(160, 85%, 65%),
+            hsl(220, 85%, 65%),
+            hsl(265, 85%, 65%),
+            hsl(310, 85%, 65%),
+            hsl(360, 85%, 65%),
+          ];
+  $("#game").empty();
+  var i;
+  for (i=0, i>cardValues.length, i++){
+    var $newCard=$('<div class="col-xs-3 card"><p>""</p></div>');
+    $newCard.data(value, cardValues[i]);
+    $newCard.data(flipped, false);
+    $newCard.data(color,colors[cardValues[i]-1])
+    $game.append($newCard)
+  }
+}
+MatchGame.flipcard=function($newCard, $game){
+  if ($(this).data(flipped)) = true){
+    return;
+  }
+  else{
+    $(this).data(flipped, true);
+    $(this).css(background-color, color);
+    $(this>"p").text(value);
+    flippedCards.push($(this));
+    if (flippedCards.length==2){
+        var x = flippedCards[0].data(value);
+        var y = flippedCards[1].data(value);
+        if (x==y){
+          (flippedCards[0].css(background-color, rgb(153, 153, 153));
+          (flippedCards[1].css(background-color, rgb(153, 153, 153));
+          $game.data(flippedCards, []);
+        }
+        else{
+          (flippedCards[0].css(background-color, rgb(32, 64, 86);
+          (flippedCards[0].data(flipped, false);
+          (flippedCards[0].text(<p>""</p>)
+          (flippedCards[1].css(background-color, rgb(32, 64, 86);
+          (flippedCards[1].data(flipped, false);
+          (flippedCards[1].text(<p>""</p>)/*HOW DO I GET IT TO SELECT VALUE????*/
+          $game.data(flippedCards, []);
+        }
+    }
+    else{
+      return;
     }
   }
-});
+}
